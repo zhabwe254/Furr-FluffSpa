37cba038cbe4d552be4055a8f34d7872924d2bb4
Tshepiso Rosa <132429932+Mafuyeka@users.noreply.github.com>
2024-06-12 23:38:34 +0100

    Delete backend/src/utils/authUtils.js

--- backend/src/utils/authUtils.js
+++
@@ -1,7 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const generateAuthToken = (user) => {
-    return jwt.sign({ user }, process.env.JWT_SECRET, { expiresIn: '1h' });
-};
-
-module.exports = { generateAuthToken };
