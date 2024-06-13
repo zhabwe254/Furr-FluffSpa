babbfaedd4f6241bac25d461da03d78c0d8be5ef
zhabwe254 <zhabwe@gmail.com>
2024-06-14 02:48:45 +0300

    a

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
     db: {
-        uri: process.env. mongodb+srv://gmarkd:GKpmZZ9A5wJGT3lB@cluster0.jiyzrgy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+        uri: process.env.mongodb+srv://gmarkd:GKpmZZ9A5wJGT3lB@cluster0.jiyzrgy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
     },
     port: process.env.PORT || 3000
 };
