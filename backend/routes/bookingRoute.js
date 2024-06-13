479f51404164b92d37bbd4270cab47d57ace39fb
tshepisoroshina@gmail.com <tshepisoroshina@gmail.com>
2024-06-13 05:23:01 -0700

    routes

---
+++ backend/routes/bookingRoute.js
@@ -0,0 +1,13 @@
+// bookingRoute.js
+
+const express = require('express');
+const router = express.Router();
+const bookingController = require('../controllers/bookingController');
+
+router.get('/', bookingController.getAllBookings);
+router.post('/', bookingController.createBooking);
+router.get('/:id', bookingController.getBookingById);
+router.put('/:id', bookingController.updateBooking);
+router.delete('/:id', bookingController.deleteBooking);
+
+module.exports = router;
---
+++ backend/routes/customerRoute.js
@@ -0,0 +1,13 @@
+// customerRoute.js
+
+const express = require('express');
+const router = express.Router();
+const customerController = require('../controllers/customerController');
+
+router.get('/', customerController.getAllCustomers);
+router.post('/', customerController.createCustomer);
+router.get('/:id', customerController.getCustomerById);
+router.put('/:id', customerController.updateCustomer);
+router.delete('/:id', customerController.deleteCustomer);
+
+module.exports = router;
