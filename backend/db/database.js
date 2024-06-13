aaecd157f9c9a86bb096b14e3bfc251890023ebd
tshepisoroshina@gmail.com <tshepisoroshina@gmail.com>
2024-06-13 12:14:30 -0700

    Updated

--- backend/db/database.js
+++ backend/db/database.js
@@ -1,19 +1,14 @@
 const mongoose = require('mongoose');
-require('dotenv').config(); // Load environment variables
 
 const connectDB = async () => {
   try {
-    const uri = process.env.MONGODB_URI;
-    if (!uri) {
-      throw new Error('MONGODB_URI is not defined in the environment variables');
-    }
-    await mongoose.connect(uri, {
+    await mongoose.connect('DB_URI=mongodb://Tshepiso:%40Tshepiso1971@localhost:27017/mydatabase', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('Connected to the database');
+    console.log('Database connected successfully');
   } catch (error) {
-    console.error('MongoDB connection error:', error.message);
+    console.error('Database connection faild:', error.message);
     process.exit(1); // Exit process on connection failure
   }
 };
