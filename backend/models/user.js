5a9829b307beb4dd2c24d1e40fc3a30fcd891c86
tshepisoroshina@gmail.com <tshepisoroshina@gmail.com>
2024-06-17 20:26:53 -0700

    Updated

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,11 +1,24 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  phone: {
+    type: String,
+    required: true
+  },
+  pet: {
     name: String,
-    email: String,
-    password: String,
+    type: String,
+    age: Number
+  }
 });
 
-const User = mongoose.model('User', userSchema);
-
-module.exports = User;
+module.exports = mongoose.model('User', userSchema);
